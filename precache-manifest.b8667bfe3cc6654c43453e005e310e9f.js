59da512408c3444c0dada40ee9adef4ec7488b7e
Rizel Scarlett <blackgirlbytes@github.com>
2021-12-11 09:50:53 +0000

    Updates

---
+++ precache-manifest.b8667bfe3cc6654c43453e005e310e9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ac5b233070bd2a4062b",
+    "url": "/countdown-champ/static/css/main.7ec8a306.chunk.css"
+  },
+  {
+    "revision": "7ac5b233070bd2a4062b",
+    "url": "/countdown-champ/static/js/main.7ac5b233.chunk.js"
+  },
+  {
+    "revision": "d0ed89b3661a63809a32",
+    "url": "/countdown-champ/static/js/1.d0ed89b3.chunk.js"
+  },
+  {
+    "revision": "4c75989000fe23fe2741",
+    "url": "/countdown-champ/static/js/runtime~main.4c759890.js"
+  },
+  {
+    "revision": "12dbac8fdf5b02cf4908018bd58a54e5",
+    "url": "/countdown-champ/index.html"
+  }
+];
